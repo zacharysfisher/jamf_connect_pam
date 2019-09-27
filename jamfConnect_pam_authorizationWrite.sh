529d50f628961013ce217e21d66d8ed421329257
Zachary Fisher <zacharysfisher@gmail.com>
2019-09-27 17:58:22 -0400

    Update jamfConnect_pam_authorizationWrite.sh

--- jamfConnect_pam_authorizationWrite.sh
+++ jamfConnect_pam_authorizationWrite.sh
@@ -7,6 +7,8 @@ JAMF_BINARY="/usr/local/bin/jamf"
 pamPath=/Library/Application\ Support/JAMF/PAM
 authFile=/Library/Application\ Support/JAMF/PAM/authorization_list.txt
 prefsTool=/Users/Shared/prefs.py
+pamModule="/usr/local/lib/pam/pam_saml.so.2"
+sudoConfig=$(cat /etc/pam.d/sudo | grep "pam_saml.so" | awk '{print $3}')
 
 # Create Backup Directory if it does not exist
 if [ -d "$pamPath" ] 
@@ -34,7 +36,27 @@ else
 	$JAMF_BINARY policy -event prefsTool
 fi
 
+# Checks for PAM Module
+if [ -f "$pamModule" ]; then
+	echo "PAM Module Installed"
+else
+	echo "PAM Module not installed, installing..."
+	$JAMF_BINARY policy -event pammodule
+fi
 
+# Rewrites Sudo Authorization
+if [ $sudoConfig = "pam_saml.so" ]; then
+	echo "PAM Configured"
+else
+	echo "Configuring PAM..."
+	echo "Copying Sudo File"
+	cp /etc/pam.d/sudo /etc/pam.d/sudo.bak
+	echo "Inserting new line"
+	sed -e '/# sudo: auth account password session/a\'$'\n''auth       sufficient     pam_saml.so' /etc/pam.d/sudo.bak > /etc/pam.d/sudo
+	echo "Editing Permissions on new File"
+	chmod 444 /etc/pam.d/sudo
+	chown root:wheel /etc/pam.d/sudo
+fi
 
 # Create Backup of Jamf Connect Mechanism
 security authorizationdb read com.jamf.connect.sudosaml > /Library/Application\ Support/JAMF/PAM/sudosaml.org
