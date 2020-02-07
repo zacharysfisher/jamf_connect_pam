b233ea6b7e60824d4b884bfc2bd595ec5e6f35f5
Zachary Fisher <zacharysfisher@gmail.com>
2020-02-07 16:03:29 -0500

    Update jamfconnect_pam_authorizationWrite_v1.sh

--- jamfconnect_pam_authorizationWrite_v1.sh
+++ jamfconnect_pam_authorizationWrite_v1.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#!/bin/bash
+
 # This deployment will automatically write all authorizations and make backups.  This is not controlled via a plist or configuration profile.
 
 # variables
@@ -10,12 +12,26 @@ authFile=/Library/Application\ Support/JAMF/PAM/authorization_list.txt
 pamModule=/usr/local/lib/pam/pam_saml.so.2
 sudoConfig=$(cat /etc/pam.d/sudo | grep "pam_saml.so" | awk '{print $3}')
 
+
+# Write Authorization Function
+writeJamfAuthorization()
+ {
+	for authorization in $authorizations
+	do
+		echo "Backing up Default Authorizations"
+		security authorizationdb read "${authorization}" > /Library/Application\ Support/JAMF/PAM/backup/$authorization.bak
+		echo "Writing Jamf Connect Mechanism to $authorization}"
+		security authorizationdb write "${authorization}" < /Library/Application\ Support/JAMF/PAM/sudosaml.org
+	done
+}
+
 # Create Backup Directory if it does not exist
 if [ -d "$pamPath" ] 
 then
 	echo "PAM Directory exists." 
 else
 	echo "Error: PAM Directory does not exists."
+	echo "Creating Directory"
 	mkdir -p /Library/Application\ Support/JAMF/PAM/backup
 fi
 
@@ -33,10 +49,10 @@ if [ -f "$pamModule" ]; then
 	echo "PAM Module Installed"
 else
 	echo "PAM Module not installed, installing..."
-	$JAMF_BINARY policy -event "Jamf Connect Login Trigger"
+	$JAMF_BINARY policy -event "Jamf Connect Login-enrollment"
 fi
 
-# Rewrites Sudo Authorization
+# Rewrites Sudo Authorization / Remove Local Auth
 if [ $sudoConfig = "pam_saml.so" ]; then
 	echo "PAM Configured"
 else
@@ -44,28 +60,32 @@ else
 	echo "Copying Sudo File"
 	cp /etc/pam.d/sudo /etc/pam.d/sudo.bak
 	echo "Inserting new line"
-	sed -e '/# sudo: auth account password session/a\'$'\n''auth       sufficient     pam_saml.so' /etc/pam.d/sudo.bak > /etc/pam.d/sudo
+	sed -e '/# sudo: auth account password session/a\'$'\n''auth       required       pam_saml.so' /etc/pam.d/sudo.bak > /etc/pam.d/sudo 
 	echo "Editing Permissions on new File"
 	chmod 444 /etc/pam.d/sudo
 	chown root:wheel /etc/pam.d/sudo
+	echo "Clean up backup file"
+	rm -rf /etc/pam.d/sudo.bak
+	# Disable Local Auth
+	echo "Copying Sudo File"
+	cp /etc/pam.d/sudo /etc/pam.d/sudo.bak
+	echo "Remove local auth"
+	sed 's/auth       required       pam_opendirectory.so/#auth      required       pam_opendirectory.so/' /etc/pam.d/sudo.bak > /etc/pam.d/sudo 
+	echo "Editing Permissions on new File"
+	chmod 444 /etc/pam.d/sudo
+	chown root:wheel /etc/pam.d/sudo
+	echo "Clean up backup file"
+	rm -rf /etc/pam.d/sudo.bak
 fi
 
 # Create Backup of Jamf Connect Mechanism / remove NoCache
+echo "Creating Backup of JamfConnect Mech - Removing NoCache"
 security authorizationdb read com.jamf.connect.sudosaml > /Library/Application\ Support/JAMF/PAM/sudosaml.org
 sed -i -e 's/AuthUINoCache/AuthUI/g' /Library/Application\ Support/JAMF/PAM/sudosaml.org
 
 # Write new Mechanism to jamf connect
+echo "Write new Mech to Jamf Folder"
 security authorizationdb read com.jamf.connect.sudosaml < /Library/Application\ Support/JAMF/PAM/sudosaml.org
 
-# Write Authorization Function
-function writeJamfAuthorization {
-	for authorization in $authorizations
-	do
-		echo "Backing up Default Authorizations"
-		security authorizationdb read "${authorization}" > /Library/Application\ Support/JAMF/PAM/backup/$authorization.bak
-		echo "Writing Jamf Connect Mechanism to $authorization}"
-		echo "security authorizationdb write "${authorization}" < /Library/Application\ Support/JAMF/PAM/sudosaml.org"
-	done
-}
-
+echo "Authorization Function"
 writeJamfAuthorization
